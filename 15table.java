1a16c619c6b5c7c7a47c907994550e09ebc2e109
vinithvinith <124652883+vinithvinith@users.noreply.github.com>
2023-02-06 21:02:06 -0800

    print the 15th table in java

---
+++ 15table.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class 15table
+{
+public static void main(String[] args)
+{
+int n,i;
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+for(i=1;i<=10;i++)
+{
+ans=n*i
+}
+System.out.println(ans);
+}
+}
